90f662b11b53f0b170ea7619486b3b4b67a397b7
FangyiChao <34549328+FannyChao@users.noreply.github.com>
2019-01-08 17:29:27 +0100

    Add files via upload

---
+++ scripts/02-train.py
@@ -0,0 +1,192 @@
+#   Two mode of training available:
+#       - BCE: CNN training, NOT Adversarial Training here. Only learns the generator network.
+#       - SALGAN: Adversarial Training. Updates weights for both Generator and Discriminator.
+#   The training used data previously  processed using "01-data_preocessing.py"
+import os
+import numpy as np
+import sys
+import cPickle as pickle
+import random
+import cv2
+import theano
+import theano.tensor as T
+import lasagne
+
+from tqdm import tqdm
+from constants import *
+from models.model_salgan import ModelSALGAN
+from models.model_bce import ModelBCE
+from utils import *
+import pdb
+
+
+#flag = str(sys.argv[1])
+#lr = str(sys.argv[2])
+flag = 'salgan'
+
+def bce_batch_iterator(model, train_data, validation_sample):
+    num_epochs = 301 # fy: change from 301
+    n_updates = 1
+    nr_batches_train = int(len(train_data) / model.batch_size)
+    for current_epoch in tqdm(range(num_epochs), ncols=20):
+        e_cost = 0.
+
+        random.shuffle(train_data)
+
+        for currChunk in chunks(train_data, model.batch_size):
+
+            if len(currChunk) != model.batch_size:
+                continue
+
+            batch_input = np.asarray([x.image.data.astype(theano.config.floatX).transpose(2, 0, 1) for x in currChunk],
+                                     dtype=theano.config.floatX)
+
+            batch_output_sal = np.asarray([y.saliency.data.astype(theano.config.floatX) / 255. for y in currChunk],
+                                      dtype=theano.config.floatX)
+            batch_output_sal = np.expand_dims(batch_output_sal, axis=1)
+            
+            batch_output_fixa = np.asarray([z.fixation.data.astype(theano.config.floatX) / 255. for z in currChunk],
+                                      dtype=theano.config.floatX)
+            batch_output_fixa = np.expand_dims(batch_output_fixa, axis=1)
+
+            # train generator with one batch and discriminator with next batch
+            G_cost = model.G_trainFunction(batch_input, batch_output_sal, batch_output_fixa)
+            e_cost += G_cost
+            n_updates += 1
+
+        e_cost /= nr_batches_train
+
+        print 'Epoch:', current_epoch, ' train_loss->', e_cost
+
+        if current_epoch % 5 == 0:
+            np.savez(DIR_TO_SAVE + '/ft30_1_mlr_gen_modelWeights{:04d}.npz'.format(current_epoch),
+                     *lasagne.layers.get_all_param_values(model.net['output']))
+            predict(model=model, image_stimuli=validation_sample, num_epoch=current_epoch, path_output_maps=DIR_TO_SAVE)
+
+
+def salgan_batch_iterator(model, train_data, validation_sample):
+    num_epochs = 301 # fy: change from 301
+    nr_batches_train = int(len(train_data) / model.batch_size)
+    n_updates = 1
+    for current_epoch in tqdm(range(num_epochs), ncols=20):
+        
+        g_cost = 0.
+        d_cost = 0.
+        e_cost = 0.
+
+        random.shuffle(train_data)
+
+        for currChunk in chunks(train_data, model.batch_size):
+
+            if len(currChunk) != model.batch_size:
+                continue
+            
+            batch_input = np.asarray([x.image.data.astype(theano.config.floatX).transpose(2, 0, 1) for x in currChunk],
+                                     dtype=theano.config.floatX)
+            
+            batch_output_sal = np.asarray([y.saliency.data.astype(theano.config.floatX) / 255. for y in currChunk],
+                                      dtype=theano.config.floatX)
+            batch_output_sal = np.expand_dims(batch_output_sal, axis=1)
+            
+            batch_output_fixa = np.asarray([z.fixation.data.astype(theano.config.floatX) / 255. for z in currChunk],
+                                      dtype=theano.config.floatX)
+            batch_output_fixa = np.expand_dims(batch_output_fixa, axis=1)
+            
+            #pdb.set_trace()
+            '''
+            batch_output_sal_re = np.asarray([cv2.resize(y.saliency.data.astype(theano.config.floatX), (int(y.fixation.data.astype(theano.config.floatX)[0,0]),int(y.fixation.data.astype(theano.config.floatX)[0,1])),interpolation=cv2.INTER_CUBIC ) / 255. for y in currChunk],
+                                      dtype=theano.config.floatX)
+            batch_output_sal_re = np.expand_dims(batch_output_sal_re, axis=1)
+            
+            batch_output_fixa = np.asarray([z.fixation.data.astype(theano.config.int_division) for z in currChunk],
+                                      dtype=theano.config.int_division)
+            
+            batch_output_fixa_map = np.asarray([np.zeros( (int(z.fixation.data.astype(theano.config.floatX)[0,0]),int(z.fixation.data.astype(theano.config.floatX)[0,1]))) for z in currChunk])
+            
+            #batch_output_fixa_map = np.asarray([ batch_output_fixa_map[i, batch_output_fixa[i] ]    for i in model.batch_size]  )
+            
+            for i in range(model.batch_size):
+                fixa_map = batch_output_fixa_map[i]
+                fixa =  batch_output_fixa[i, 1:302]
+                k=[k for k in fixa if k[0] >0 ]
+                fixa_map[k[0]-1, k[1]-1]=1
+                batch_output_fixa_map[i]=fixa_map
+            
+            batch_output_fixa_map = np.expand_dims(batch_output_fixa_map, axis=1)
+    
+            #batch_output2 = np.expand_dims(batch_output2, axis=1)
+            '''
+            
+            # train generator with one batch and discriminator with next batch
+            if n_updates % 2 == 0:
+                G_obj, D_obj, G_cost = model.G_trainFunction(batch_input, batch_output_sal, batch_output_fixa)
+                #G_obj, D_obj, G_cost = model.G_trainFunction(batch_input, batch_output_sal)
+                d_cost += D_obj
+                g_cost += G_obj
+                e_cost += G_cost
+            else:
+                G_obj, D_obj, G_cost = model.D_trainFunction(batch_input, batch_output_sal, batch_output_fixa)
+                #G_obj, D_obj, G_cost = model.D_trainFunction(batch_input, batch_output_sal)
+                d_cost += D_obj
+                g_cost += G_obj
+                e_cost += G_cost
+
+            n_updates += 1
+
+        g_cost /= nr_batches_train
+        d_cost /= nr_batches_train
+        e_cost /= nr_batches_train
+        # pdb.set_trace()
+        # Save weights every 3 epoch
+        if current_epoch % 10 == 0:
+            np.savez(DIR_TO_SAVE + '/HE_video/model8/weights1/gen_modelWeights{:04d}.npz'.format(current_epoch),
+                     *lasagne.layers.get_all_param_values(model.net['output']))
+            np.savez(DIR_TO_SAVE + '/HE_video/model8/weights1/discrim_modelWeights{:04d}.npz'.format(current_epoch),
+                     *lasagne.layers.get_all_param_values(model.discriminator['prob']))
+            predict(model=model, image_stimuli=validation_sample, num_epoch=current_epoch, name='val_model8', path_output_maps=DIR_TO_SAVE)
+        print 'Epoch:', current_epoch, ' train_loss->', (g_cost, d_cost, e_cost)
+
+
+def train():
+    """
+    Train both generator and discriminator
+    :return:
+    """
+    # Load data
+    print 'Loading training data...'
+    #with open('../saliency-2016-lsun/validationSample240x320.pkl', 'rb') as f:
+    with open(TRAIN_DATA_DIR, 'rb') as f:
+        train_data = pickle.load(f)
+    print '-->done!'
+
+    print 'Loading validation data...'
+    # with open('../saliency-2016-lsun/validationSample240x320.pkl', 'rb') as f:
+    with open(VAL_DATA_DIR, 'rb') as f:
+        validation_data = pickle.load(f)
+    print '-->done!'
+
+    # Choose a random sample to monitor the training
+    num_random = random.choice(range(len(validation_data)))
+    validation_sample = validation_data[num_random]
+    cv2.imwrite(DIR_TO_SAVE + '/validationRandomSaliencyGT.png', validation_sample.saliency.data)
+    cv2.imwrite(DIR_TO_SAVE + '/validationRandomImage.png', cv2.cvtColor(validation_sample.image.data,
+                                                                                cv2.COLOR_RGB2BGR))
+
+    # Create network
+
+    if flag == 'salgan':
+        model = ModelSALGAN(INPUT_SIZE[0], INPUT_SIZE[1])
+        # Load a pre-trained model
+        load_weights(net=model.net['uconv2_1'], path="gen_", epochtoload=90, layernum=48) # full layernum=54
+        load_weights(net=model.discriminator['prob'], path='discrim_', epochtoload=90, layernum=20) # full layernum=20
+        salgan_batch_iterator(model, train_data, validation_sample.image.data)
+
+    elif flag == 'bce':
+        model = ModelBCE(INPUT_SIZE[0], INPUT_SIZE[1])
+        # Load a pre-trained model
+        load_weights(net=model.net['output'], path='gen_', epochtoload=90) # fy:load pretrained BCE model
+        bce_batch_iterator(model, train_data, validation_sample.image.data)
+    else:
+        print "Invalid input argument."
+if __name__ == "__main__":
+    train()
